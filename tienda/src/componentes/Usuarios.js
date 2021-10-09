d3893fd00322a3ce8e04d1a4375d636f5993bdb8
José Miguel Castellanos <jose.castellanos@sistemasaberes.com>
2021-10-08 23:31:23 -0500

    usuario component

---
+++ tienda/src/componentes/Usuarios.js
@@ -0,0 +1,329 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
+
+const data = [
+  { id: 1, usuario: "Ana01 ", apellido: "Lopez", nombre: "Ana", correo: "ana01@gmail.com"},
+  { id: 2, usuario: "maria12 ", apellido: "Luna", nombre: "Maria", correo: "maria12@hotmail.com"},
+  { id: 3, usuario: "Kevin56", apellido: "Ruiz" , nombre: "Kevin", correo: "kevin56@gmail.com"},
+  { id: 4, usuario: "lola45", apellido: "Olivares",nombre: "Lola", correo:"lola45@gmail.com"  },
+  { id: 5, usuario: "Edward45 ", apellido: "Luna",nombre:"Edwar", correo:"edwar45@gmail.com" },
+  { id: 6, usuario: "Luis45 ", apellido: "Lugo", nombre: "Luis", correo:"luis45@outlook.com" },
+];
+
+class Usuarios extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      usuario: "",
+      apellido: "",
+      nombre: "",
+      correo:"",
+    },
+  };
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.id == registro.id) {
+        arreglo[contador].usuario = dato.usuario;
+        arreglo[contador].apellido = dato.apellido;
+        arreglo[contador].nombre = dato.nombre;
+        arreglo[contador].correo = dato.correo;
+
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
+    if (opcion == true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id == registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
+  insertar= ()=>{
+    var valorNuevo= {...this.state.form};
+    valorNuevo.id=this.state.data.length+1;
+    var lista= this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    
+    return (
+      <>
+        <Container>
+        <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Nuevo Usuario</Button>
+          <br />
+          <br />
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Usuario</th>
+                <th>Apellido</th>
+                <th>Nombre</th>
+                <th>Correo</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.usuario}</td>
+                  <td>{dato.apellido}</td>
+                  <td>{dato.nombre}</td>
+                  <td>{dato.correo}</td>
+
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+           <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
+            
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Usuario: 
+              </label>
+              <input
+                className="form-control"
+                name="usuario"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.usuario}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Apellido: 
+              </label>
+              <input
+                className="form-control"
+                name="apellido"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.anime}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="apellido"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.anime}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Correo: 
+              </label>
+              <input
+                className="form-control"
+                name="correo"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.anime}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+
+
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+           <div><h3>Insertar Usuario</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+                Id: 
+              </label>
+              
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length+1}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Usuario: 
+              </label>
+              <input
+                className="form-control"
+                name="usuario"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Apellido: 
+              </label>
+              <input
+                className="form-control"
+                name="apellido"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="nombre"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Correo: 
+              </label>
+              <input
+                className="form-control"
+                name="correo"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
+}
+export default Usuarios;
