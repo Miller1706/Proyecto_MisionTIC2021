3bafa3141a5fed65b248669a819c4b64feb02332
José Miguel Castellanos <jose.castellanos@sistemasaberes.com>
2021-10-10 23:06:45 -0500

    conexion bd

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Usuarios from './componentes/Usuarios';
 import Ventas from './componentes/Ventas';
 import Productos from './componentes/Productos';
+import firebase from './firebase';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
    <>
+   
    <Usuarios></Usuarios>
     <Ventas></Ventas>
     <Productos></Productos>
