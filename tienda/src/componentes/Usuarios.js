80060c39bc7e386601d13cc9bd243e0bfdefde24
LuisaFGO <fgiraldo070@gmail.com>
2021-10-23 21:50:45 -0500

    Usuarios con busqueda

--- tienda/src/componentes/Usuarios.js
+++ tienda/src/componentes/Usuarios.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import fireDb from "../firebase"
+import fireDb from "../firebase";
+import MaterialTable from 'material-table';
+
 import {
-  Table,
   Button,
   Container,
   Modal,
@@ -12,7 +13,45 @@ import {
   ModalFooter,
 } from "reactstrap";
 
+import { forwardRef } from 'react';
 
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+
+const tableIcons = {
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 class Usuarios extends React.Component {
   state = {
@@ -26,7 +65,9 @@ class Usuarios extends React.Component {
       correo:"",
     },
     id:0
+    
   };
+  
   peticionGet = () => {
     fireDb.child("usuarios").on("value", (usuario) => {
       if (usuario.val() !== null) {
@@ -68,9 +109,9 @@ class Usuarios extends React.Component {
     }})
     console.log(this.state.form);
   }
-  seleccionarUsuario=async(usuario,id,caso)=>{
+  seleccionarUsuario=async(usuario,caso)=>{
 
-    await this.setState({form: usuario, id: id});
+    await this.setState({form: usuario, id: usuario.id});
 
     (caso==="Editar")?this.setState({modalEditar: true}):
     this.peticionDelete()
@@ -88,35 +129,62 @@ class Usuarios extends React.Component {
           <Button color="success" onClick={()=>this.setState({modalInsertar: true})}>Nuevo Usuario</Button>
           <br />
           <br />
-          <Table className="table table-bordered">
-            <thead>
-              <tr>
-                
-                <th>Identificacion</th>
-                <th>Apellido</th>
-                <th>Nombre</th>
-                <th>Correo</th>
-                <th>Acción</th>
-              </tr>
-            </thead>
+          <MaterialTable
+          columns={[
+            { 
+              title: "Identificacion", 
+              field: "identificacion"
+            },
+            { 
+              title: "Nombre",
+              field: "nombre" 
+            }, 
+            {
+              title: "Apellido",
+              field: "apellido"
+            },  
+            {
+              title: "Correo",
+              field: "correo"
+            },
+            
+          ]}
 
-            <tbody>
-              {Object.keys(this.state.data).map(i => {
-                return <tr key={i}>
-                  
-                  <td>{this.state.data[i].identificacion}</td>
-                  <td>{this.state.data[i].apellido}</td>
-                  <td>{this.state.data[i].nombre}</td>
-                  <td>{this.state.data[i].correo}</td>
-                  <td>
-                    <Button color="primary" onClick={() => this.seleccionarUsuario(this.state.data[i],i,'Editar')}>Editar</Button>{" "}
-                    <Button color="danger" onClick={()=> this.seleccionarUsuario(this.state.data[i],i,'Eliminar')}>Eliminar</Button>
-                  </td>
+          data= {Object.keys(this.state.data).map(i => {
+                return{
+                  id:i,
+                  identificacion:this.state.data[i].identificacion,
+                  apellido:this.state.data[i].apellido,
+                  nombre:this.state.data[i].nombre,
+                  correo:this.state.data[i].correo
+                }
+              })}
+              title="Lista de usuarios"
+              icons={tableIcons}  
+              actions={[
+                {
+                  icon: EditIcon,
+                  tooltip: 'Editar usuario',
+                  onClick: (event, rowData) => this.seleccionarUsuario(rowData, "Editar")
+                },
+                {
+                  icon: DeleteIcon,
+                  tooltip: 'Eliminar usuario',
+                  onClick: (event, rowData) => this.seleccionarUsuario(rowData, "Eliminar")
+                }
+              ]}
+            
 
-                </tr>
-               })}
-            </tbody>
-          </Table>
+              
+              
+                 // <td>
+                   // <Button color="primary" onClick={() => this.seleccionarUsuario(this.state.data[i],i,'Editar')}>Editar</Button>{" "}
+                    //<Button color="danger" onClick={()=> this.seleccionarUsuario(this.state.data[i],i,'Eliminar')}>Eliminar</Button>
+                  //</td>
+
+                //</tr>
+               
+              />
         </Container>
 
         <Modal isOpen={this.state.modalEditar}>
